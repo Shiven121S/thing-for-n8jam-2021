11f7d0fac6036b9d8cec2104f78fc17c1da2fc13
LudumStudios <65925982+Shiven121S@users.noreply.github.com>
2021-08-10 18:59:31 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,16 +3,121 @@ namespace SpriteKind {
     export const Target = SpriteKind.create()
     export const Particle = SpriteKind.create()
 }
+function Levels () {
+    if (Level == 1) {
+        game.splash("Press enter to restart the level at any time.")
+        scene.setTileMap(img`
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 f f f f f f f f f f f 7 7 7 7 
+            7 7 7 7 7 f 7 7 7 7 7 7 7 7 7 f 7 7 7 7 
+            7 7 7 7 7 f 7 7 7 7 7 7 7 7 7 f 7 7 7 7 
+            7 7 7 7 7 f c 7 7 4 7 7 7 3 7 f 7 7 7 7 
+            7 7 7 7 7 f 7 7 7 7 7 7 7 7 7 f 7 7 7 7 
+            7 7 7 7 7 f f f f f f f f f f f 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            `, TileScale.Eight)
+        AddTilesAndSprites()
+    } else if (Level == 2) {
+        game.splash("Press enter to restart the level at any time.")
+        scene.setTileMap(img`
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 f f f f f f f f f f f 7 7 7 7 
+            7 7 7 7 7 f 7 7 7 7 7 7 7 7 7 f 7 7 7 7 
+            7 7 7 7 7 f c 7 4 7 7 f 7 7 3 f 7 7 7 7 
+            7 7 7 7 7 f 7 7 7 7 7 7 7 7 7 f 7 7 7 7 
+            7 7 7 7 7 f 7 7 7 7 7 7 7 7 7 f 7 7 7 7 
+            7 7 7 7 7 f f f f f f f f f f f 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            `, TileScale.Eight)
+        AddTilesAndSprites()
+    } else if (Level == 3) {
+        game.splash("Press enter to restart the level at any time.")
+        scene.setTileMap(img`
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 7 f f f f f f f f 7 7 7 7 7 7 7 7 7 7 
+            7 7 f 7 7 7 7 7 7 f f f f f f f f 7 7 7 
+            7 7 f c 7 7 7 7 7 f f 7 7 7 7 7 f 7 7 7 
+            7 7 f 7 7 7 7 f 7 f 7 7 7 7 7 7 f 7 7 7 
+            7 7 f f f f f f 7 7 7 7 7 f 7 7 f 7 7 7 
+            7 7 7 7 7 7 7 f 7 4 7 f 7 7 7 7 f 7 7 7 
+            7 7 7 7 7 7 7 f 7 7 7 7 7 7 7 7 f 7 7 7 
+            7 7 7 7 7 7 7 f f f f f f f f 7 f 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 7 f 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 3 f 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 7 f 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 f f f 7 7 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            `, TileScale.Eight)
+        AddTilesAndSprites()
+    } else if (Level == 4) {
+        game.splash("Press enter to restart the level at any time.")
+        scene.setTileMap(img`
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 f f f f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 f c 7 7 7 f 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 f 7 7 7 7 f 7 7 7 7 7 7 7 7 7 7 7 7 7 
+            7 f f f f 7 f 7 7 f f f f 7 7 7 7 7 7 7 
+            7 7 7 7 f 7 f f f f 7 7 f f f f f f 7 7 
+            7 7 7 7 f 7 7 7 7 7 7 7 f 7 7 f 7 f 7 7 
+            7 7 7 7 f 7 4 f 7 7 f 7 7 7 7 7 7 f f 7 
+            7 7 7 7 f 7 7 f 7 7 7 7 7 7 7 7 7 7 f 7 
+            7 7 7 7 f f f f 7 7 7 f 7 7 7 7 7 7 f 7 
+            7 7 7 7 7 7 7 f f f f f 7 7 f f f 7 f 7 
+            7 7 7 7 7 7 7 7 7 7 7 f f f f f f 7 f 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 f f 7 7 f f 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 7 7 3 7 
+            7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f f f f f 
+            `, TileScale.Eight)
+        AddTilesAndSprites()
+    }
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    moveUp()
+})
 function moveRight () {
     if (!(blockMenu.isMenuOpen())) {
         if (!(mySprite.isHittingTile(CollisionDirection.Right))) {
-            if (Sokobox2.x == mySprite.x + 8) {
+            if (Sokobox2.x == mySprite.x + 8 && Sokobox2.y == mySprite.y) {
                 if (!(Sokobox2.isHittingTile(CollisionDirection.Right))) {
                     mySprite.x += 8
                     Sokobox2.x += 8
                 }
             } else {
                 mySprite.x += 8
+            }
+        }
+    }
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (!(blockMenu.isMenuOpen())) {
+        Levels()
+    }
+})
+function moveUp () {
+    if (!(blockMenu.isMenuOpen())) {
+        if (!(mySprite.isHittingTile(CollisionDirection.Top))) {
+            if (Sokobox2.y == mySprite.y - 8 && Sokobox2.x == mySprite.x) {
+                if (!(Sokobox2.isHittingTile(CollisionDirection.Right))) {
+                    mySprite.y += -8
+                    Sokobox2.y += -8
+                }
+            } else {
+                mySprite.y += -8
             }
         }
     }
@@ -27,47 +132,68 @@ function SetColors () {
     color.setColor(4, color.rgb(199, 143, 82))
     color.setColor(12, color.rgb(46, 29, 48))
 }
+sprites.onOverlap(SpriteKind.Sokobox, SpriteKind.Target, function (sprite, otherSprite) {
+    Level += 1
+    console.log(Level)
+    Levels()
+    music.smallCrash.play()
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     moveLeft()
 })
+function moveDown () {
+    if (!(blockMenu.isMenuOpen())) {
+        if (!(mySprite.isHittingTile(CollisionDirection.Bottom))) {
+            if (Sokobox2.y == mySprite.y + 8 && Sokobox2.x == mySprite.x) {
+                if (!(Sokobox2.isHittingTile(CollisionDirection.Right))) {
+                    mySprite.y += 8
+                    Sokobox2.y += 8
+                }
+            } else {
+                mySprite.y += 8
+            }
+        }
+    }
+}
+function fadeThing () {
+    color.setColor(7, color.rgb(38, 36, 51), 750)
+    timer.after(755, function () {
+        color.setColor(7, color.rgb(130, 172, 149), 750)
+    })
+}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     moveRight()
 })
 function AddTilesAndSprites () {
-    if (Math.percentChance(40)) {
-        scene.setTile(15, img`
-            7 7 7 f 7 7 7 f 
-            7 7 7 f 7 7 7 f 
-            f f f f f f f f 
-            f 7 7 7 f 7 7 7 
-            f 7 7 7 f 7 7 7 
-            f f f f f f f f 
-            7 7 7 f 7 7 7 f 
-            f f f f f f f f 
-            `, true)
-    } else if (Math.percentChance(25)) {
-        scene.setTile(15, img`
-            7 7 7 f 7 7 7 f 
-            f 7 7 f f 7 7 f 
-            f f f f f f f f 
-            f 7 7 7 f 7 7 f 
-            f f 7 7 f 7 7 7 
-            f f f f f f f f 
-            7 7 7 f 7 7 7 f 
-            f f f f f f f f 
-            `, true)
-    } else if (Math.percentChance(35)) {
-        scene.setTile(15, img`
-            7 7 7 f 7 7 7 f 
-            7 7 7 f 7 7 7 f 
-            f f f f f f f f 
-            f 7 7 7 f 7 7 7 
-            f f f f f f f f 
-            7 7 7 f 7 7 7 f 
-            7 7 7 f 7 7 7 f 
-            f f f f f f f f 
-            `, true)
-    }
+    Bricks = [img`
+        7 7 7 f 7 7 7 f 
+        7 7 7 f 7 7 7 f 
+        f f f f f f f f 
+        f 7 7 7 f 7 7 7 
+        f 7 7 7 f 7 7 7 
+        f f f f f f f f 
+        7 7 7 f 7 7 7 f 
+        f f f f f f f f 
+        `, img`
+        7 7 7 f 7 7 7 f 
+        f 7 7 f f 7 7 f 
+        f f f f f f f f 
+        f 7 7 7 f 7 7 f 
+        f f 7 7 f 7 7 7 
+        f f f f f f f f 
+        7 7 7 f 7 7 7 f 
+        f f f f f f f f 
+        `, img`
+        7 7 7 f 7 7 7 f 
+        7 7 7 f 7 7 7 f 
+        f f f f f f f f 
+        f 7 7 7 f 7 7 7 
+        f f f f f f f f 
+        7 7 7 f 7 7 7 f 
+        7 7 7 f 7 7 7 f 
+        f f f f f f f f 
+        `]
+    scene.setTile(15, Bricks._pickRandom(), true)
     scene.setTile(7, img`
         f f f f f f f f 
         f f f f f f f f 
@@ -112,10 +238,13 @@ function AddTilesAndSprites () {
     scene.placeOnRandomTile(Sokobox2, 4)
     scene.placeOnRandomTile(Target2, 3)
 }
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    moveDown()
+})
 function moveLeft () {
     if (!(blockMenu.isMenuOpen())) {
         if (!(mySprite.isHittingTile(CollisionDirection.Right))) {
-            if (Sokobox2.x == mySprite.x - 8) {
+            if (Sokobox2.x == mySprite.x - 8 && Sokobox2.y == mySprite.y) {
                 if (!(Sokobox2.isHittingTile(CollisionDirection.Right))) {
                     mySprite.x += -8
                     Sokobox2.x += -8
@@ -179,8 +308,7 @@ blockMenu.onMenuOptionSelected(function (option, index) {
             ffffffffffffffffffffffffffffffffffffffffffffffff
             `)
         game.setDialogTextColor(7)
-        game.showLongText("Made in 72 Hours for the N8Jam. The game is a sokoban esque puzzle game set in the distant future run by robotic overlord. Your goal is to complete the puzzles to break into an unconscious ", DialogLayout.Center)
-        game.showLongText("robotic guard's firewalls, and eventually break the system. Move the virus to the targets to disable the firewalls. If you hear an echoing thump, that should serve as an reminder that the unconcious robot is active. And though your mission isn't over, you have to flee.", DialogLayout.Center)
+        game.showLongText("Made in 4 days for the first N8Jam. You play as a character that has just escaped a digital consciousness, where billions of people are currently trapped. You must solve sokoban-esque puzzles to break the system, and relieve the billions of souls trapped. If you fail to finish the puzzles within the time limit, you alert the system and will hear the thumping of footsteps at your door, reminding you that your mission ins't over, but have failed for now.", DialogLayout.Center)
     } else if (option == "How to play") {
         game.setDialogFrame(img`
             ffffffffffffffffffffffffffffffffffffffffffffffff
@@ -233,29 +361,12 @@ blockMenu.onMenuOptionSelected(function (option, index) {
             ffffffffffffffffffffffffffffffffffffffffffffffff
             `)
         game.setDialogTextColor(7)
-        game.showLongText("Move with WASD or the arrow keys. Use WASD or arrow keys to push the boxed virus around, and into the target to move to the next level. ", DialogLayout.Center)
+        game.showLongText("Move with WASD or the arrow keys. Use WASD or arrow keys to push the boxed virus around, and into the target to move to the next level. Attempt to avoid overlapping with the brick wall, but you can cheat if you are stuck.", DialogLayout.Center)
     } else {
         color.setColor(7, color.rgb(38, 36, 51), 750)
         timer.after(755, function () {
             Level = 1
             glitchesExe.destroy()
-            scene.setTileMap(img`
-                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-                7 7 7 7 7 f f f f f f f f f f f 7 7 7 7 
-                7 7 7 7 7 f 7 7 7 7 7 7 7 7 7 f 7 7 7 7 
-                7 7 7 7 7 f 7 7 7 7 7 7 7 7 7 f 7 7 7 7 
-                7 7 7 7 7 f c 7 7 4 7 7 7 3 7 f 7 7 7 7 
-                7 7 7 7 7 f 7 7 7 7 7 7 7 7 7 f 7 7 7 7 
-                7 7 7 7 7 f f f f f f f f f f f 7 7 7 7 
-                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-                7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-                `, TileScale.Eight)
             mySprite = sprites.create(img`
                 . 4 4 4 4 4 4 . 
                 4 4 . . . . 4 4 
@@ -266,6 +377,7 @@ blockMenu.onMenuOptionSelected(function (option, index) {
                 4 . . . . . . . 
                 . 4 4 4 4 4 4 4 
                 `, SpriteKind.Player)
+            mySprite.setPosition(120, 16)
             Sokobox2 = sprites.create(img`
                 . 4 . . . . 4 . 
                 4 4 4 4 4 4 4 4 
@@ -276,6 +388,7 @@ blockMenu.onMenuOptionSelected(function (option, index) {
                 4 4 4 4 4 4 4 4 
                 . 4 . . . . 4 . 
                 `, SpriteKind.Sokobox)
+            Sokobox2.setPosition(40, 70)
             Target2 = sprites.create(img`
                 3 3 3 3 3 3 3 3 
                 3 . . . . . . 3 
@@ -286,20 +399,23 @@ blockMenu.onMenuOptionSelected(function (option, index) {
                 3 . . . . . . 3 
                 3 3 3 3 3 3 3 3 
                 `, SpriteKind.Target)
-            AddTilesAndSprites()
-            timer.after(50, function () {
-                blockMenu.closeMenu()
-                blockMenu.setControlsEnabled(false)
-                color.setColor(7, color.rgb(130, 172, 149), 750)
+            Target2.setPosition(70, 54)
+            blockMenu.closeMenu()
+            blockMenu.setControlsEnabled(false)
+            color.setColor(7, color.rgb(130, 172, 149), 750)
+            timer.after(755, function () {
+                Levels()
             })
         })
     }
 })
-let Level = 0
 let Target2: Sprite = null
+let Bricks: Image[] = []
 let Sokobox2: Sprite = null
 let mySprite: Sprite = null
+let Level = 0
 let glitchesExe: Sprite = null
+console.log("x")
 blockMenu.showMenu(["Play", "How to play", "About"], MenuStyle.List, MenuLocation.BottomHalf)
 blockMenu.setColors(15, 7)
 glitchesExe = sprites.create(img`
